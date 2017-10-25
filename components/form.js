7debc329ecf3b1a516c11968a2596ba96cd6f6bb
Marejke Schulz <m.schulz@strictmobile.de>
2017-10-25 17:52:39 +0200

    add all input fields and submit button

--- components/form.js
+++ components/form.js
@@ -13,6 +13,9 @@ const Input = styled.input`
 const Label = styled.label`
 `
 
+const Button = styled.button`
+`
+
 export default class RegistrationForm extends Component {
 
 	generateFieldUpdater(fieldName) {
@@ -20,10 +23,26 @@ export default class RegistrationForm extends Component {
 			const value = e.target.value;
 			const errors = this.state.errors;
 			if (['firstName', 'lastName'].includes(fieldName) && !value.match(/^[a-zA-Z]+$/)) {
-				errors[fieldName] = 'Your name is invalid';
+				errors[fieldName] = 'The entered name is invalid';
 				this.setState({ errors });
 				return;
 			}
+			if (fieldName === 'userName' && !value.match(/^[a-z0-9._]+$/)) {
+				errors[fieldName] = 'The entered username is invalid';
+				this.setState({ errors });
+				return;
+			}
+			if (fieldName === 'password' && !value.length <= 8) {
+				errors[fieldName] = 'The entered password is too short';
+				this.setState({ errors });
+				return;
+			}
+			if (fieldName === 'email' && !value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+				errors[fieldName] = 'The entered email address is invalid';
+				this.setState({ errors });
+				return;
+			}
+
 			delete errors[fieldName];
 			this.setState({
 				fields: {
@@ -41,9 +60,12 @@ export default class RegistrationForm extends Component {
 			errors: {},
 			fields: {
 				firstName: '',
-				lastName: ''
+				lastName: '',
+				userName: '',
+				password: '',
+				email: ''
 			}
-		}
+		};
 		this.generateFieldUpdater = this.generateFieldUpdater.bind(this);
 	}
 
@@ -64,9 +86,36 @@ export default class RegistrationForm extends Component {
 						onKeyUp={this.generateFieldUpdater('lastName')}
 						type={'text'}
 						id={'last-name'}
-						placeholder={'Please insert your first name'}
+						placeholder={'Please insert your last name'}
 					/>
 					<p>{this.state.errors.lastName}</p>
+					<Label htmlFor={'user-name'}>Username</Label>
+					<Input
+						onKeyUp={this.generateFieldUpdater('userName')}
+						type={'text'}
+						id={'user-name'}
+						placeholder={'Please insert a username'}
+					/>
+					<p>{this.state.errors.userName}</p>
+					<Label htmlFor={'last-name'}>Password</Label>
+					<Input
+						onKeyUp={this.generateFieldUpdater('password')}
+						type={'password'}
+						id={'password'}
+						placeholder={'Please insert a password'}
+					/>
+					<p>{this.state.errors.password}</p>
+					<Label htmlFor={'email'}>Email</Label>
+					<Input
+						onKeyUp={this.generateFieldUpdater('email')}
+						type={'email'}
+						id={'email'}
+						placeholder={'Please insert your email address'}
+					/>
+					<p>{this.state.errors.email}</p>
+					<Button type={'submit'}>
+						Submit
+					</Button>
 
 				</FormWrapper>
 			</div>
